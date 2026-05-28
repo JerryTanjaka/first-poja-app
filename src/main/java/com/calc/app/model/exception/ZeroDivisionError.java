19207d5fadb81d7dad08e6c3a0475fd4d3aedfe6
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-05-29 02:50:28 +0300

    feat : add zero divisionererror number model exception

---
+++ src/main/java/com/calc/app/model/exception/ZeroDivisionError.java
@@ -0,0 +1,7 @@
+package com.calc.app.model.exception;
+
+public class ZeroDivisionError extends RuntimeException {
+    public ZeroDivisionError() {
+        super("Cannot divide by zero");
+    }
+}
