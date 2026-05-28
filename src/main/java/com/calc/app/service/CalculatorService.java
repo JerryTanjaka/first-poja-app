1b09f9d9e2ab164031b63aa6a43fe7d565a6c7c9
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-05-29 02:35:36 +0300

    feat : add basic arith method

---
+++ src/main/java/com/calc/app/service/CalculatorService.java
@@ -0,0 +1,29 @@
+package com.calc.app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public double add(double a, double b) {
+        validate(a, b);
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        validate(a, b);
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        validate(a, b);
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        validate(a, b);
+        if (b == 0) {
+            throw new ZeroDivisionError();
+        }
+        return a / b;
+    }
+}
